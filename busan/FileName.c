b4917d20c2a870e3314e8704ad95930b1cea2866
Vksxk <dwk03289@naver.com>
2024-05-11 00:39:36 +0900

    과제

--- busan/FileName.c
+++ busan/FileName.c
@@ -2,10 +2,31 @@
 #include <Windows.h>
 #include <stdlib.h>
 #include <time.h>
-#define LEN_MIN 15             
+
+//파라미터 
+#define LEN_MIN 15 // 기차 길이             
 #define LEN_MAX 50
-#define PROB_MIN 10        
+#define STM_MIN 0 // 마동석 체력
+#define STM_MAX 5
+#define PROB_MIN 10 // 확률        
 #define PROB_MAX 90
+#define AGGRO_MIN 0 // 어그로 범위
+#define AGGRO_MAX 5
+
+// 마동석 이동 방향
+#define MOVE_LEFT 1
+#define MOVE_STAY 0
+
+// 좀비의 공격 대상
+#define ATK_NONE 0
+#define ATK_CITIZEN 1
+#define ATK_DONGSEOK 2
+
+// 마동석 행동
+#define ACTION_REST 0
+#define ACTION_PROVOKE 1
+#define ACTION_PULL 2
+
 
 int main(void) {
 	srand((unsigned int)time(NULL));
